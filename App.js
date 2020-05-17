153fe7cb742b602449eb01bca37e6f9265ecc414
Nai <bapdola304@gmail.com>
2020-05-17 15:09:28 +0700

    move source code from expo to CLI

--- App.js
+++ App.js
@@ -1,114 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import  BottomNavigation  from './navigation';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  render() {
+    return (
+      <NavigationContainer>
+        <StatusBar
+          barStyle="dark-content"
+          // dark-content, light-content and default
+          hidden={false}
+          //To hide statusBar
+          backgroundColor="#f7f9fb"
+          //Background color of statusBar
+          translucent={false}
+          //allowing light, but not detailed shapes
+          networkActivityIndicatorVisible={true}
+        />
+        <BottomNavigation />
+      </NavigationContainer>
+    );
+  }
+}
 
 export default App;
+// import * as React from 'react';
+// import { Text, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+// function Feed() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Feed!</Text>
+//     </View>
+//   );
+// }
+
+// function Profile() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Profile!</Text>
+//     </View>
+//   );
+// }
+
+// function Notifications() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Notifications!</Text>
+//     </View>
+//   );
+// }
+
+// const Tab = createMaterialBottomTabNavigator();
+
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator
+//       initialRouteName="Feed"
+//       activeColor="#e91e63"
+//       labelStyle={{ fontSize: 12 }}
+//       style={{ backgroundColor: 'tomato' }}
+//     >
+//       <Tab.Screen
+//         name="Feed"
+//         component={Feed}
+//         options={{
+//           tabBarLabel: 'Home',
+//           tabBarIcon: ({ color }) => (
+//             <MaterialCommunityIcons name="home" color={color} size={26} />
+//           ),
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Notifications"
+//         component={Notifications}
+//         options={{
+//           tabBarLabel: 'Updates',
+//           tabBarIcon: ({ color }) => (
+//             <MaterialCommunityIcons name="bell" color={color} size={26} />
+//           ),
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Profile"
+//         component={Profile}
+//         options={{
+//           tabBarLabel: 'Profile',
+//           tabBarIcon: ({ color }) => (
+//             <MaterialCommunityIcons name="account" color={color} size={26} />
+//           ),
+//         }}
+//       />
+//     </Tab.Navigator>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <MyTabs />
+//     </NavigationContainer>
+//   );
+// }
+
---
+++ navigation/index.js
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import { Text, View, Image } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import circlePlusIcon from '../assets/plus-circle.png';
+import  ProductComponent  from '../components/Product.component';
+import OrderComponent from '../components/Order/Order.component';
+import AccountComponent from '../components/Account.component';
+
+function Feed() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+BottomNavigation = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Shopping"
+      activeColor="#5860ef"
+      labelStyle={{ fontSize: 12 }}
+      inactiveColor="#bfcad9"
+      barStyle={{ backgroundColor: '#ffffff' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Feed}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <Icon name='home' color={color} size={26}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Shopping"
+        component={ProductComponent}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <Icon name="shopping-cart" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CirclePlus"
+        component={Notifications}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            // <Icon color={color} size={30} name = "plus-circle-outline"/>
+            <Image source = {circlePlusIcon} style = {{width: 50, height: 50, marginTop: -10}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={OrderComponent}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            // <Icon name="cart-outline" color={color} size={26} />
+            <Icon name="event-note" color={color} size={26}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountComponent}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <Icon name="account-box" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default BottomNavigation;
+
+
